860447942d68d63dff77a1b7b1994614f1d83f72
Weston Voglesonger <tvogle@ad.unc.edu>
2022-08-26 10:42:55 -0400

    Finished Conditionals Lesson!

--- lessons/input.py
+++ lessons/input.py
@@ -1,4 +1,5 @@
 """Demonstrates asking the user for input."""
 
-print("Hello, " + input("What is your name?") + ", good morning!")
-
+user_name: str = input("What is your name?")
+print("Hello, " + user_name + ", good morning!")
+print("You are awesome, " + user_name)
